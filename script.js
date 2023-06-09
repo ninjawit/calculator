07ce0a8ec3d70a7a8095fcfb67018b8dd2e12fcd
ninjawit <80984772+ninjawit@users.noreply.github.com>
2023-06-09 15:07:55 -0500

    Further styling and add basic number button functionality

--- script.js
+++ script.js
@@ -4,6 +4,55 @@ var operator;
 
 const display = document.querySelector("#display");
 const clear = document.querySelector("#clear");
+const numbers = document.querySelectorAll(".number");
+
+// Move switch statement to own function, then wrap switch function call in if statement that checks for text length of display before adding another number
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        switch(number) {
+            case document.getElementById("one"):
+                display.innerHTML += '1';
+                break;
+
+            case document.getElementById("two"):
+                display.innerHTML += '2';
+                break;
+
+            case document.getElementById("three"):
+                display.innerHTML += '3';
+                break;
+
+            case document.getElementById("four"):
+                display.innerHTML += '4';
+                break;
+
+            case document.getElementById("five"):
+                display.innerHTML += '5';
+                break;
+
+            case document.getElementById("six"):
+                display.innerHTML += '6';
+                break;
+
+            case document.getElementById("seven"):
+                display.innerHTML += '7';
+                break;
+
+            case document.getElementById("eight"):
+                display.innerHTML += '8';
+                break;
+
+            case document.getElementById("nine"):
+                display.innerHTML += '9';
+                break;
+
+            case document.getElementById("zero"):
+                display.innerHTML += '0';
+                break;
+        }
+    });
+});
 
 function clearDisplay() {
     display.innerHTML = '';
@@ -13,7 +62,7 @@ function clearDisplay() {
     return display;
 }
 
-clear.addEventListener("click", clearDisplay);
+clear.addEventListener('click', clearDisplay);
 
 function add(a, b) {
     return a + b;
