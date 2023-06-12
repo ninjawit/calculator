325794271810891f318048846fbe4ed5f520cecd
ninjawit <80984772+ninjawit@users.noreply.github.com>
2023-06-12 14:27:24 -0500

    Begin operator and equals button functionality

--- script.js
+++ script.js
@@ -1,68 +1,98 @@
 var firstNumber;
 var secondNumber;
-var operator;
+var sign;
 
 const display = document.querySelector("#display");
 const clear = document.querySelector("#clear");
 const numbers = document.querySelectorAll(".number");
-
-// Move switch statement to own function, then wrap switch function call in if statement that checks for text length of display before adding another number
+const operators = document.querySelectorAll(".operate");
+const equals = document.querySelector("#equals");
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        switch(number) {
+        numberClick(number);
+    });
+});
+
+function numberClick(numberButton) {
+    if (display.innerHTML.length < 15){
+        switch(numberButton) {
             case document.getElementById("one"):
                 display.innerHTML += '1';
                 break;
-
+    
             case document.getElementById("two"):
                 display.innerHTML += '2';
                 break;
-
+    
             case document.getElementById("three"):
                 display.innerHTML += '3';
                 break;
-
+    
             case document.getElementById("four"):
                 display.innerHTML += '4';
                 break;
-
+    
             case document.getElementById("five"):
                 display.innerHTML += '5';
                 break;
-
+    
             case document.getElementById("six"):
                 display.innerHTML += '6';
                 break;
-
+    
             case document.getElementById("seven"):
                 display.innerHTML += '7';
                 break;
-
+    
             case document.getElementById("eight"):
                 display.innerHTML += '8';
                 break;
-
+    
             case document.getElementById("nine"):
                 display.innerHTML += '9';
                 break;
-
+    
             case document.getElementById("zero"):
                 display.innerHTML += '0';
                 break;
         }
-    });
-});
+    }
+}
+
+clear.addEventListener('click', clearDisplay);
 
 function clearDisplay() {
     display.innerHTML = '';
     firstNumber = '';
     secondNumber = '';
-    operator = '';
+    sign = '';
     return display;
 }
 
-clear.addEventListener('click', clearDisplay);
+/* Current one step behind and doesn't display right with multiple operators.  Eg. 9 * 9 - 6 displays as 9*96 = 81 Also want to figure out how to not clear display until start entering in second number
+*/
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        sign = operator.innerHTML;
+        if (firstNumber && secondNumber) {
+            operate(firstNumber, secondNumber, sign);
+        }
+        else if (firstNumber) { // not empty, null, undefined
+            secondNumber = Number(display.innerHTML);
+        }
+        else {
+            firstNumber = Number(display.innerHTML);
+            display.innerHTML = '';
+        }
+    });
+});
+
+// Currently returns NaN
+equals.addEventListener('click', () => {
+    operate(firstNumber, secondNumber, sign);
+    firstNumber = Number(display.innerHTML);
+})
 
 function add(a, b) {
     return a + b;
@@ -81,18 +111,22 @@ function divide(a, b) {
 }
 
 function operate(num1, num2, opr) {
+    var result;
+
     switch(opr) {
         case '+':
-            return add(num1, num2);
+            result = add(num1, num2);
             break;
         case '-':
-            return subtract(num1, num2);
+            result = subtract(num1, num2);
             break;
         case '*':
-            return multiply(num1, num2);
+            result = multiply(num1, num2);
             break;
         case '/':
-            return divide(num1, num2);
+            result = divide(num1, num2);
             break;
     }
+    display.innerHTML = result;
+    firstNumber = result;
 }
